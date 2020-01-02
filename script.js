e3ac8147e9b807fa865fad87533e49c5aa68c1b5
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-01-02 09:08:21 -0300

    small update

---
+++ script.js
@@ -0,0 +1,25 @@
+let menuHome = document.getElementById('menuHome')
+let menuAutoral = document.getElementById('menuAutoral')
+let menuProjetos = document.getElementById('menuProjetos')
+let menuReforma = document.getElementById('menuReforma')
+let menuInteriores = document.getElementById('menuInteriores')
+let menuConsultoria = document.getElementById('menuConsultoria')
+let menuQuemSomos = document.getElementById('menuQuemSomos')
+let menuContato = document.getElementById('menuContato')
+let instagram = document.getElementById('instagram')
+let facebook = document.getElementById('facebook')
+
+instagram.onclick = function () {
+
+    window.location = 'https://www.instagram.com/sararporto/'
+    
+}
+
+
+facebook.onclick = function () {
+
+    window.location = 'https://www.facebook.com/sara.porto.984'
+    
+}
+
+
